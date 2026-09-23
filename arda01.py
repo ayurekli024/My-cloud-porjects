d938bee575d1d8860c8250c873e4eaf076a6de56
ayurekli024 <yurekliarda24@gmail.com>
2026-09-23 14:09:49 +0300

    Update input prompt for name extraction script

--- arda01.py
+++ arda01.py
@@ -1,3 +1,4 @@
-fullName = input("Enter")
+fullName = input("Enter Your Name:")
 n = fullName.rfind(" ")
-print("Last Name:", fullName[n+1:])
+print("Last Name:", fullName.upper()[n+1:])
+print("First Name:", fullName.capitalize()[:n])
